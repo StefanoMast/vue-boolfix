1ffe344acf1e0b519b5e8f7b9e3f9399b1e5b702
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-23 17:31:42 +0200

    add scaffolding, header and search input

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+      <div class="header">
+          <h1>boolfix</h1>
+          <input type="text" placeholder="ciao"> 
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+    components: {
+    
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.header {
+    background-color: darkgrey;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h1 {
+    text-transform: uppercase;
+    color: red;
+    }
+    
+
+}
+
+</style>
