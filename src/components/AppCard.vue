9894fa455517d3a7b4c9ad126c7c2602d2f9abb7
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-26 16:05:28 +0200

    add Milestone4

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,36 +1,30 @@
 <template>
   <li class="card">
       <div class="card-inner">
-          <div class="card-image">
-            <img class="card-img" v-if="cardObj.poster_path"
-                :src="(`https://image.tmdb.org/t/p/w342${posterPath}`)"
-                alt=""
-            />
-            <img class="card-empty"
-            v-else
-            src="../assets/img/immagine-vuota.jpg"
-            alt="Poster_not_available"
-            />
+          <div class="card-front">
+              <img 
+               :src="posterUrl" alt="">
           </div>
-          <h2 class="title">{{cardTitle}}</h2>
-          <h4 class="orginal-title">{{cardOriginalTitle}}</h4>
-          <!-- se la lingua ha la bandiera faccio vedere l'immagine -->
-          
-          <img 
-          v-if="languageHasImage" class="language-flag" 
-          :src="require(`../assets/img/${cardObj.original_language}.png`)" 
-          :alt="cardObj.original_language"
-          />
-        
-          <p v-else class="language">{{cardObj.original_language}}</p>
-          <p class="vote">Voto: {{filledStars}}</p>
-          <p>
-              <i v-for="n in filledStars" :key="n" class="fas fa-star" :class="n <= 5 ? 'fas' : 'far'">
-    <!-- //se n è minore o uguale al voto, allora metto *, altrimenti metto # -->
-              </i>
-          </p>
-          
-    </div> 
+          <div class="card-back">
+           
+            <h2 class="title">{{cardTitle}}</h2>
+            <h4 class="orginal-title">{{cardOriginalTitle}}</h4>
+            <!-- se la lingua ha la bandiera faccio vedere l'immagine -->
+            
+            <img 
+            v-if="languageHasImage" class="language-flag" 
+            :src="require(`../assets/img/${cardObj.original_language}.png`)" 
+            :alt="cardObj.original_language"
+            />
+            
+            <p v-else class="language">{{cardObj.original_language}}</p>
+            <p class="vote">Voto: {{filledStars}}</p>
+            <p>
+                <i v-for="n in filledStars" :key="n" class="fas fa-star" :class="n <= 5 ? 'fas' : 'far'">
+                </i>
+            </p>
+          </div>
+      </div> 
   </li>
 </template>
 
@@ -58,8 +52,8 @@ export default {
     filledStars() {
         return Math.ceil(this.cardObj.vote_average / 2);
     },
-    posterPath() {
-         return this.cardObj.poster_path;
+    posterUrl() {
+         return this.cardObj.poster_path ? `https://image.tmdb.org/t/p/w342${this.cardObj.poster_path}` : require('../assets/img/immagine-vuota.jpg')
      }
     }
 
@@ -70,25 +64,44 @@ export default {
 @import '~@fortawesome/fontawesome-free/css/all.min.css';
 
 .card {
-  width: calc(100%/5 - 8px);
+  width: calc(100%/4 - 8px);
+  height: 300px;
   margin: 4px;
   padding: 1rem;
   text-align: center;
-    
+  perspective: 1000px;
+}
+
+.card:hover {
+    transform: rotateY(180deg);
+}
+
+.card-inner {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background-color: black;
+    color: white;
+    transform-style: preserve-3d;
+    transition: transform .5s;
 }
 
 .language-flag {
     width: 2rem;
 }
 
-.card-img {
+.card-front, .card-back {
     width: 100%;
-    height: 300px;
+    height: 100%;
+    position: absolute;
+    backface-visibility: hidden;
 }
 
-.card-empty {
+.card-front img {
     width: 100%;
-    height: 300px;
 }
 
+.card-back {
+    transform: rotateY(180deg);
+}
 </style>
